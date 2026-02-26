37368aae403665b88e2801fc389db1fee98d554d
Emrah Tek <swissemrah@gmail.com>
2026-02-26 23:11:55 +0100

    name wird andern

--- 11_Masking.py
+++ 11_Masking.py
@@ -10,15 +10,11 @@ cv.imshow('Blank Image', blank)
 circle = cv.circle(blank, ( img.shape[1]//2,img.shape[0]//2), 100,255,-1)
 cv.imshow('Mask', circle)
 
-
 rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255,-1)
 
 weird_shape = cv.bitwise_and(circle,rectangle)
 
 masked = cv.bitwise_and(img,img, mask = weird_shape)
 cv.imshow('Masked Image', masked)
-
-
-
 
 cv.waitKey(0)
