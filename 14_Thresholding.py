d51514ccfb1d456bfe57f5d7d97a187972692634
Emrah Tek <swissemrah@gmail.com>
2026-02-27 23:43:53 +0100

    Thresholding _ 2

--- 14_Thresholding.py
+++ 14_Thresholding.py
@@ -11,4 +11,16 @@ cv.imshow('GRay', gray)
 threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
 cv.imshow('Simple Threshold', thresh)
 
-cv.waitKey(0)
+# Simple Thresholding inverse
+threshold, thresh_inv= cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Threshold Inverse', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
+
+# Adaptive Thresholding Inverse
+adaptive_thresh_inv = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 11, 3)
+cv.imshow('Adaptive Thresholding Iverse', adaptive_thresh_inv)
+
+cv.waitKey(0) 
